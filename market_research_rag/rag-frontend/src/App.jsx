17f279f43c35167a216fe286586d0afb6bbc7531
Danish <danm44@hotmail.com>
2026-02-06 15:59:07 -0500

    created a navbar. doesnt work

--- market_research_rag/rag-frontend/src/App.jsx
+++ market_research_rag/rag-frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import RagChat from "./components/rag/RagChat";
 import QueryBox from "./components/QueryBox";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="app-container">
+      <Navbar />
       <h1>RAG</h1>
-
       <div className="fullscreen-box">
         <QueryBox />
       </div>
